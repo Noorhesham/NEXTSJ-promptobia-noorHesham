3bb655b235ff6c680a565ecff619b5e1eaa41648
noor <noordragon2004@gmail.com>
2024-02-09 05:22:24 -0800

    fix

--- utils/database.js
+++ utils/database.js
@@ -8,7 +8,7 @@ export const connectToDB=async ()=>{
         return;
     }
     try {
-        await mongoose.connect(process.env.MONGO_URI,{
+        await mongoose.connect('mongodb+srv://noorboi:promptnoor55@cluster0.r58tukl.mongodb.net/?retryWrites=true&w=majority',{
             dbName:'share_prompt',
             useNewUrlParser:true,
             useUnifiedTopology:true,
